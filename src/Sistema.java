3866e1f9e7f5cfb8dfe96af4661c93e7faa761b8
Josias Neves Jardim Borba <jojarbor3@gmail.com>
2024-12-10 04:51:21 -0300

    Sistema class finished, I think? Needs testing.

--- src/Sistema.java
+++ src/Sistema.java
@@ -1,7 +1,14 @@
-import java.security.PublicKey;
 import java.util.ArrayList;
 
 public class Sistema {
+    /*
+    private ArrayList<Aluno> alunos;
+    private ArrayList<Admin> adms;
+    private ArrayList<Produto> prods;
+    private ArrayList<Pedido> pedidos;
+    private ArrayList<Sala> salas;
+    */
+
     ArrayList<Aluno> alunos;
     ArrayList<Admin> adms;
     ArrayList<Produto> prods;
@@ -14,7 +21,63 @@ public class Sistema {
         this.prods = new ArrayList<>();
         this.pedidos = new ArrayList<>();
         this.salas = new ArrayList<>();
-        // Inserir dados de teste
+
+    }
+
+    public boolean sistemaVazio() {
+        return this.adms.isEmpty();
+
+    }
+
+    public String gerarCodigoProduto(){
+        return "PROD-" + this.prods.size() + 1;
+
+    }
+
+    public String gerarCodigoPedido(){
+        return "PEDIDO-" + this.pedidos.size() + 1;
+
+    }
+
+    public void listarProdutos(){
+        for (Produto prod : this.prods) {
+            System.out.println(prod.toString() + "\n");
+
+        }
+
+    }
+
+    public void listarSalas(){
+        for (Sala sala : this.salas) {
+            System.out.println(sala.toString() + "\n");
+
+        }
+
+    }
+
+    public ArrayList<Pedido> filtrarPedidos(Boolean disponivel){
+        ArrayList<Pedido> pedidos_filtrados = new ArrayList<>();
+
+        for(Pedido p : this.pedidos){
+            if(p.disponivel() == disponivel) pedidos_filtrados.add(p);
+
+        }
+
+        return pedidos_filtrados;
+
+    }
+
+    //NEED TO CHECK IF IT WORKS
+    public ArrayList<Pedido> filtrarPedidos(Aluno a){
+        ArrayList<Pedido> pedidos_filtrados = new ArrayList<>();
+
+        for(Pedido p : this.pedidos){
+            if(p.getCliente().equals(a)) pedidos_filtrados.add(p);
+
+        }
+
+        return pedidos_filtrados;
+
     }
 
     public void addAdmin(Admin a) {
@@ -37,73 +100,56 @@ public class Sistema {
         this.salas.add(s);
     }
 
+    /*** GETS ***/
+
     public Aluno getAluno(String cpf) {
         for(Aluno a : this.alunos) {
             if (cpf.equals(a.getCPF())) return a;
+
         }
 
         return null;
+
     }
 
     public Admin getAdmin(String cpf) {
         for(Admin a : this.adms) {
             if (cpf.equals(a.getCPF())) return a;
+
         }
 
         return null;
+
     }
 
-    /*** GETS ***/
-
-    /*
-
     public Produto getProduto(String cod){
+        for(Produto prod : this.prods) {
+            if(cod.equals(prod.getCodigo())) return prod;
+
+        }
+
+        return null;
 
     }
 
     public Pedido getPedido(String cod){
+        for(Pedido p : this.pedidos) {
+            if(cod.equals(p.getCod())) return p;
+
+        }
+
+        return null;
 
     }
 
+    //NEED TO CHECK IF IT WORKS
     public Sala getSala(String nome){
+        for (Sala s : this.salas) {
+            if(nome.equals(s.toString())) return s;
+
+        }
+
+        return null;
 
     }
-
-
-     */
-
-    public boolean sistemaVazio() {
-        return this.adms.size() == 0;
-    }
-
-    /*
-    public String gerarCodigoProduto(){
-
-    }
-
-    public String gerarCodigoPedido(){
-
-    }
-
-    public void listarProdutos(){
-
-    }
-
-    public void listarSalas(){
-
-    }
-
-    public ArrayList<Pedidos> filtrarPedidos(Boolean disponivel){
-
-
-    }
-
-    public ArrayList<Pedidos> filtrarPedidos(Aluno a){
-
-
-    }
-
-
-
-     */
 }
